67141c336ecff793aad11d285097e8db9a43c131
nankim <seggimarugira@gmail.com>
2022-01-30 18:20:59 +0200

    home screen

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -32,7 +32,7 @@ class _LoginPageState extends State<LoginPage> {
                     onPressed: () {},
                     icon: Icon(
                       Icons.language_outlined,
-                      color: Colors.deepOrange,
+                      color: Colors.deepOrangeAccent,
                     ))),
           ),
           fkAuthWidgets.authTopContent(itemList: [
--- lib/screens/screens/content_box_widgets.dart
+++ lib/screens/screens/content_box_widgets.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fuko_app/widgets/shared/style.dart';
 
-class FkAuthWidgets {
+class FkContentBoxWidgets {
   static Widget body(context, {List<Widget> itemList = const []}) {
     return Scaffold(
       body: SafeArea(
@@ -27,32 +26,19 @@ class FkAuthWidgets {
     );
   }
 
-  Widget authTopContent({List<Widget> itemList = const []}) {
+  Widget initialItems({List<Widget> itemList = const []}) {
     return Container(
-      margin: EdgeInsets.only(top: 20),
       padding: EdgeInsets.all(20.0),
       child: Align(
           alignment: Alignment.bottomLeft,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  "Fuko",
-                  style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.w600,
-                      color: fkBlackText),
-                ),
-              ),
-              ...itemList
-            ],
+            children: [...itemList],
           )),
     );
   }
 
-  static Widget authInputFieldBox(context, {List<Widget> itemList = const []}) {
+  static buttonsItemsBox(contex, {List<Widget> itemList = const []}) {
     return Expanded(
       child: Container(
           child: NotificationListener<OverscrollIndicatorNotification>(
@@ -62,7 +48,7 @@ class FkAuthWidgets {
         },
         child: SingleChildScrollView(
           physics: ClampingScrollPhysics(),
-          padding: EdgeInsets.all(20.0),
+          padding: EdgeInsets.only(left: 20.0, right: 20.0),
           child: Form(
             autovalidateMode: AutovalidateMode.onUserInteraction,
             child: Column(
