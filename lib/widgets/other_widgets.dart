6825b2761e084bc0df3286aaaab980dd689400d7
nankim <seggimarugira@gmail.com>
2022-02-03 04:03:25 +0200

    expense screen

--- lib/widgets/other_widgets.dart
+++ lib/widgets/other_widgets.dart
@@ -36,3 +36,60 @@ Widget homeCard({leadingIcon, currency, amount, titleTxt, fn}) {
     ),
   );
 }
+
+// Repport card
+
+Widget reportCard({leadingText, currency, amount, titleTxt, bdTxt, fn}) {
+  return Container(
+    child: Card(
+      child: ExpansionTile(
+        leading: Container(
+          height: 80.0,
+          width: 80.0,
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: fkDefaultColor,
+          ),
+          child: Column(
+            children: [
+              Icon(
+                Icons.calendar_today,
+                color: fkGreyText,
+                size: 15,
+              ),
+              Text(leadingText,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20))
+            ],
+          ),
+        ),
+        subtitle: Row(
+          children: [
+            Text(currency,
+                style: TextStyle(
+                    color: fkGreyText,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 11)),
+            horizontalSpaceTiny,
+            Text(amount,
+                style: TextStyle(
+                    color: fkGreyText,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18)),
+          ],
+        ),
+        title: Text(titleTxt,
+            style: TextStyle(
+                color: fkBlackText, fontWeight: FontWeight.w400, fontSize: 18)),
+        controlAffinity: ListTileControlAffinity.leading,
+        trailing: Icon(Icons.arrow_drop_down),
+        children: <Widget>[
+          ListTile(title: Text('$bdTxt')),
+        ],
+      ),
+    ),
+  );
+}
