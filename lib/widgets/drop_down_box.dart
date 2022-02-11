bd9daa51a552a4cc3a8e4c32a004f9e6a3c8114e
nankim <seggimarugira@gmail.com>
2022-02-11 02:58:30 +0200

    some changes

--- lib/widgets/drop_down_box.dart
+++ lib/widgets/drop_down_box.dart
@@ -14,36 +14,34 @@ class _CustomDropDownBoxState extends State<CustomDropDownBox> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          const Text(
-            "Year",
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-          ),
-          horizontalSpaceRegular,
-          DropdownButton<String>(
-            value: dropdownValue,
-            icon: const Icon(Icons.arrow_drop_down),
-            elevation: 16,
-            underline: const SizedBox(),
-            style: const TextStyle(
-                fontSize: 14, fontWeight: FontWeight.w600, color: fkGreyText),
-            onChanged: (String? newValue) {
-              setState(() {
-                dropdownValue = newValue!;
-              });
-            },
-            items: <String>['2021', '2022']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
-        ],
-      ),
+    return Row(
+      children: [
+        const Text(
+          "Year",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+        horizontalSpaceRegular,
+        DropdownButton<String>(
+          value: dropdownValue,
+          icon: const Icon(Icons.arrow_drop_down),
+          elevation: 16,
+          underline: const SizedBox(),
+          style: const TextStyle(
+              fontSize: 14, fontWeight: FontWeight.w600, color: fkGreyText),
+          onChanged: (String? newValue) {
+            setState(() {
+              dropdownValue = newValue!;
+            });
+          },
+          items: <String>['2021', '2022']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 }
