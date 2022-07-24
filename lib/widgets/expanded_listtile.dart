d3f3b18eca3a2b632a838efda2e0cea9d877b8b6
seggi SMS <seggimarugira@gmail.com>
2022-07-24 15:20:22 +0200

    add expense report

--- lib/widgets/expanded_listtile.dart
+++ lib/widgets/expanded_listtile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fuko_app/core/expense_report.dart';
 import 'package:fuko_app/widgets/shared/style.dart';
 import 'package:fuko_app/widgets/shared/ui_helper.dart';
 
@@ -13,13 +14,22 @@ class CustomExpandedListTile extends StatefulWidget {
 class _CustomExpandedListTileState extends State<CustomExpandedListTile> {
   bool _customTileExpanded = false;
 
+  // late List<MonthlyReportDetailList> retrieveMonthlyReportDetailList;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // retrieveMonthlyReportDetailList = fetchMonthlyReportDetailList();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Map datas = widget.data;
     return Card(
       elevation: 2.0,
       child: Container(
-          padding: EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               Padding(
@@ -83,101 +93,62 @@ class _CustomExpandedListTileState extends State<CustomExpandedListTile> {
             : Icons.arrow_drop_down,
       ),
       children: <Widget>[
-        ListTile(
-          leading: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Container(
-              width: 35,
-              height: 35,
-              padding: EdgeInsets.all(8.0),
-              color: fkBlueText,
-              child: FittedBox(
-                  child: Text(data['date'],
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w800,
-                          fontSize: 24))),
-            ),
-          ),
-          title: Column(
-            children: [
-              Row(
+        ListView.builder(
+          itemBuilder: (BuildContext context, index) {
+            var dateTime =
+                DateTime.parse("${data["detailsData"][index]["created_at"]}");
+            return ListTile(
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Container(
+                  width: 35,
+                  height: 35,
+                  padding: const EdgeInsets.all(8.0),
+                  color: fkBlueText,
+                  child: FittedBox(
+                      child: Text(
+                          "${dateTime.day > 10 ? dateTime.day : '0${dateTime.day}'}",
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                              fontSize: 24))),
+                ),
+              ),
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    data['currency'],
-                    style: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
-                        color: fkGreyText),
+                  Row(
+                    children: [
+                      Text(
+                        data['currency'],
+                        style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: fkGreyText),
+                      ),
+                      const SizedBox(
+                        width: 2,
+                      ),
+                      Text(
+                        "${data["detailsData"][index]["amount"]}",
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: fkBlackText),
+                      ),
+                    ],
                   ),
-                  const SizedBox(
-                    width: 2,
-                  ),
-                  Text(
-                    data['amount'],
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                        color: fkBlackText),
-                  ),
+                  verticalSpaceSmall,
+                  Text("${data["detailsData"][index]["description"]}"),
+                  verticalSpaceSmall,
                 ],
               ),
-              verticalSpaceSmall,
-              Text(data["description"]),
-              verticalSpaceSmall,
-            ],
-          ),
-        ),
-        const Divider(
-          thickness: 1,
-        ),
-        ListTile(
-          leading: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Container(
-              width: 35,
-              height: 35,
-              padding: const EdgeInsets.all(8.0),
-              color: fkBlueText,
-              child: const FittedBox(
-                  child: Text("05",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w800,
-                          fontSize: 24))),
-            ),
-          ),
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text(
-                    data['currency'],
-                    style: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
-                        color: fkGreyText),
-                  ),
-                  const SizedBox(
-                    width: 2,
-                  ),
-                  const Text(
-                    "500",
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                        color: fkBlackText),
-                  ),
-                ],
-              ),
-              verticalSpaceSmall,
-              const Text("By milk one pack"),
-              verticalSpaceSmall,
-            ],
-          ),
+            );
+          },
+          itemCount: data["detailsData"].length,
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
         ),
       ],
       onExpansionChanged: (bool expanded) {
