23cf3fd878ee3ee046c9dc6bb1f107a2bf9a5578
Seggi SMS <seggimarugira@gmail.com>
2022-09-15 21:27:42 +0200

    add slidable in expenses

--- lib/widgets/shared/style.dart
+++ lib/widgets/shared/style.dart
@@ -11,3 +11,4 @@ const Color fkInputFormColor = Color(0XFFfbfbfb);
 const Color fkInputFormBorderColor = Color(0XFFA6A6A6);
 const Color ftBtnColorBgSolid = Color(0XFF131A22);
 const Color ftBtnColorBg = Color(0XFF131A22);
+const Color updateBtnColor = Color.fromARGB(255, 48, 138, 51);
